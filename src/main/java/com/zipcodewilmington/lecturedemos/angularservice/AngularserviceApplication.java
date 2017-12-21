09f2444dd40743ac72f8606ce037f1e6a61d8544
Leon <leon@Leons-MacBook-Pro.local>
2017-12-20 19:11:46 -0500

    first commit

---
+++ src/main/java/com/zipcodewilmington/lecturedemos/angularservice/AngularserviceApplication.java
@@ -0,0 +1,12 @@
+package com.zipcodewilmington.lecturedemos.angularservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularserviceApplication.class, args);
+	}
+}
