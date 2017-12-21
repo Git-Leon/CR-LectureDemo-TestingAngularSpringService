f7372faadf9b3fb684f5e5ceb9129af3e9080393
Leon <leon@Leons-MacBook-Pro.local>
2017-12-21 10:12:41 -0500

    update

--- src/main/java/com/zipcodewilmington/lecturedemos/angularservice/domain/User.java
+++ src/main/java/com/zipcodewilmington/lecturedemos/angularservice/domain/User.java
@@ -4,38 +4,27 @@ public class User {
 
 	private Long id;
     private String firstName;
-	private String lastName;
-	private String email;
 
-	public Long getId() {
-		return id;
-	}
+	public User() {}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public User(Long id, String fname) {
+        this.id = id;
+        this.firstName = fname;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
